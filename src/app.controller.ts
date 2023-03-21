2f2f5afb05007ce74abe598336b02f06d65ac8eb
dudosyka <a.hatson22@gmail.com>
2023-03-21 20:38:05 +0300

    Excel import

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import readXlsxFile from 'read-excel-file/node';
+import { PvkEntity } from './pvk/entities/pvk.entity';
 
 @Controller()
-export class AppController {}
+export class AppController {
+  @Get('app')
+  load() {
+    readXlsxFile(process.cwd() + '/' + 'excel.xlsx').then((rows) => {
+      PvkEntity.bulkCreate(
+        rows.map((item) => ({
+          name: item[1],
+          description: item[2],
+        })),
+      );
+    });
+  }
+}
 
 //
 // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInJvbGUiOjIsImlhdCI6MTY3NzQwMjE5NywiZXhwIjoxNjc5OTk0MTk3fQ.xJrxkO3Yx56ek91zE7wZ2-8BMlAOFyGNrJHNJIKl7sE
